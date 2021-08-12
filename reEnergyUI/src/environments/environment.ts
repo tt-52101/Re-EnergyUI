62dd217ca897cdf3a26de7d658b22567ba6c90ed
Anova ui <devui@anova.tech>
2021-08-12 19:42:25 +0530

    misc from satyas branch

--- reEnergyUI/src/environments/environment.ts
+++ reEnergyUI/src/environments/environment.ts
@@ -20,12 +20,12 @@ export const environment = {
   // PaymentFor: 'live', // rrc live/test/local
   PaymentFor: 'local',
   reportUrl: 'http://localhost:49360/',
-  apiUrl: 'http://localhost:59017/api/',  // local
+  apiUrl: 'http://localhost:59017/api/',        // local
 
-  // reportUrl: 'http://aeltest.nabh.co:5004/',//test
+  // reportUrl: 'http://aeltest.nabh.co:5004/',  //test
   // apiUrl: 'http://aeltest.nabh.co:5002/api/', // test
 
-  // reportUrl: 'http://aelc.nabh.co:5004/', //live
+  // reportUrl: 'http://aelc.nabh.co:5004/',     //live
   // apiUrl: 'https://aelc.nabh.co:5002/api/',   // live
 };
 
